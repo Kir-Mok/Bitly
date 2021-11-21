e05410d206b518d5113ebc6db7bb0f3f09801cb5
Kir-Mok <85826257+Kir-Mok@users.noreply.github.com>
2021-11-21 19:06:50 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,78 @@
+from dotenv import load_dotenv
+from pathlib import Path
+from urllib.parse import urlparse
+import argparse
+import os
+import requests
+
+
+def shorten_link(url, headers):
+    api_url = 'https://api-ssl.bitly.com/v4/shorten'
+    payload = {
+        'long_url': url
+    }
+
+    response = requests.post(api_url, headers=headers, json=payload)
+    response.raise_for_status()
+
+    return response.json()['id']
+
+
+def count_clicks(short_link, headers):
+    parsed_link = urlparse(short_link)
+
+    api_url = 'https://api-ssl.bitly.com/v4/bitlinks/{}{}/clicks/summary'.format(
+        parsed_link.netloc, parsed_link.path)
+
+    response = requests.get(api_url, headers=headers)
+    response.raise_for_status()
+
+    return response.json()['total_clicks']
+
+
+def is_bitlink(link, headers):
+    parsed_link = urlparse(link)
+
+    api_url = 'https://api-ssl.bitly.com/v4/bitlinks/{}{}'.format(
+        parsed_link.netloc, parsed_link.path)
+
+    response = requests.get(api_url, headers=headers)
+    return response.ok
+
+
+def main():
+    load_dotenv()
+
+    load_dotenv(verbose=True)
+
+    env_path = Path('.') / '.env'
+    load_dotenv(dotenv_path=env_path)
+
+    secret = os.environ['TOKEN']
+
+    headers = {
+        'Authorization': secret
+    }
+
+    parser = argparse.ArgumentParser(description='Получает клики по битлинку')
+    parser.add_argument('url', help='Ссылка в дальнейшем будет сокращена')
+    args = parser.parse_args()
+
+    input_link = args.url
+
+    if is_bitlink(input_link, headers):
+        try:
+            clicks_count = count_clicks(input_link, headers)
+            print(clicks_count)
+        except requests.exceptions.HTTPError:
+            print('Невозможно подсчитать клики')
+    else:
+        try:
+            short_link = shorten_link(input_link, headers)
+            print(short_link)
+        except requests.exceptions.HTTPError:
+            print('Невозможная ссылка')
+
+
+if __name__ == '__main__':
+    main()
